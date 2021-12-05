c76d8d0a84d98965c626cf6204a180b1c96a16e8
Riki Saepulloh <rikisaepulloh27@gmail.com>
2021-12-05 09:22:41 +0700

    Tugas Intent

---
+++ LatihanPOJO/app/src/main/java/com/example/latihanpojo/MoveActivity.java
@@ -0,0 +1,16 @@
+package com.example.latihanpojo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.latihanpojo.R;
+
+public class MoveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+}
+
